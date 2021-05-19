e535daaac9a98f7e0c1b208ca77cb4f03a3a2545
Amy Walter <alw120@ic.ac.uk>
2021-05-19 16:22:30 +0100

    List updated

--- Week 3 - More functional/web-app/list.js
+++ Week 3 - More functional/web-app/list.js
@@ -18,11 +18,16 @@ and ending at end (exclusive) in steps of step.
 i.e. range(2, 5, 0.5) = [2, 2.5, 3, 3.5, 4, 4.5].
 range = (start, end, step) => array */
 
-function range(start, end, num, endpoint = true) {
-    const div = (endpoint ? (num - 1) : num);
-    const step = (end - start) / div;
-    return Array.from({length: num}, (_, i) => start + step * i);
-}
+List.range = (start, end, step) => new_array.from(
+    {length: (end - start) / step + 1}, (_, i) => start + (i * step));
+
+console.log(List.range);
+
+// function range(start, end, num, endpoint = true) {
+//     const div = (endpoint ? (num - 1) : num);
+//     const step = (end - start) / div;
+//     return Array.from({length: num}, (_, i) => start + step * i);
+// }
     // range(1,2,3) returns [ 1, 1.5, 2 ]
 
 /* 2. Write curried versions of map, includes, filter, every, some,
@@ -32,7 +37,7 @@ and on its 2nd takes an array and returns that function mapped to the array.*/
 
 //Map
 const map = (func) => (array) => array.map(func);
-console.log(map(range)([1, 2, 3, 4]));
+console.log(map(List.range)([1, 2, 3, 4]));
 
 //Fiter
 const filter = (element) => (condition) => condition.element === element;
@@ -58,37 +63,40 @@ function reject(array, iteratorFunction) {
     });
 }
 
-reject([1, 2, 3, 4], range);
+reject([1, 2, 3, 4], List.range);
 /* console.log(reject([1, 2, 3, 4, 5], iteratorFunction))
 where iterator function is any function e.g. range (an above example) */
 
 // 4. Code that returns the first element of the array, also write last
-const first = (array) => (array[0]);
-console.log(first([1, 2, 3])); // returns [1]
+List.first = (array) => (array[0]);
+console.log(List.first([1, 2, 3])); // returns [1]
 
-const last = (array) => array[array.length - 1];
-console.log(last([1, 2, 3])); // returns [3]
+List.last = (array) => array[array.length - 1];
+console.log(List.last([1, 2, 3])); // returns [3]
 
 // 5. Return an array with the first element removed.
-const rest = (array) => array.slice(1);
-console.log(rest(["a", "b", "c"])); // returns ['b', 'c']
+List.rest = (array) => array.slice(1);
+console.log(List.rest(["a", "b", "c"])); // returns ['b', 'c']
 
 // 6. Return an array with the last element removed.
-const most = (array) => array.slice(0, -1);
-console.log(most([1, 2, 3, 4, 5])); //returns [1, 2, 3, 4]
+List.most = (array) => array.slice(0, -1);
+console.log(List.most([1, 2, 3, 4, 5])); //returns [1, 2, 3, 4]
 
 /* 7. Zip is a function like map but that applies to two arrays.
 E.g. zip(g)([a, b, c])([x, y, z]) would return [g(a, x), g(b, y), g(c, z)]. */
 
-function zippp(arrays) {
-    return arrays[0].map(function (i) {
-        return arrays.map(function (array) {
-            return array[i];
-        });
-    });
-}
+List.zip = (func) => (arr1) => (arr2) => arr1.map((ignore, index) => 
+func(arr1[index], arr2[index]));
 
-console.log(zippp([1, 2, 3, 4]));
+// function zippp(arrays) {
+//     return arrays[0].map(function (i) {
+//         return arrays.map(function (array) {
+//             return array[i];
+//         });
+//     });
+// }
+
+console.log(List.zip([1, 2, 3, 4]));
 
 /* '_' is a valid variable identifier in JavaScript,
 and could theoretically refer to anything. */
